12f612596beaefbf60fee9952b28ea0dd7404d81
David Feimer <davidfeimer@gmail.com>
2015-05-05 00:37:18 +0000

    initial commit

---
+++ bet.js
@@ -0,0 +1,39 @@
+var bank = 100;
+$(document).ready(function() {
+  document.getElementById("bank").innerHTML = "Current Bank: $" + bank; 
+});
+
+function run(){
+     
+  var wager = prompt("What is your wager between $5-$10?");
+  var guess = prompt("Guess a number between 1 and 10");
+  var random = Math.floor(Math.random()*11)
+  var result = Math.abs(guess - random)
+  $(document).ready(function() {
+  document.getElementById("guess").innerHTML = "Your Guess: " + guess;
+  });
+  $(document).ready(function() {
+  document.getElementById("computer_guess").innerHTML = "Computer Guess: " + random; 
+  });
+
+  switch(result) {
+    case 0:
+    bank = bank + (wager * 2);
+    alert("You won $" + (wager * 2));
+    break;
+    case 1:
+    alert("You were off by 1 and broke even");
+    break;
+    default:
+    alert("You lose $" + wager);
+    bank -= wager;
+    break;
+  };
+
+  $(document).ready(function() {
+    document.getElementById("bank").innerHTML = "Current Bank: $" + bank;
+  });
+
+};
+
+
